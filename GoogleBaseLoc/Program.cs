1eb9981f4b3777b990ed3c5407f4ec278782a7c3
伪红学家 <wang-p12@mails.tsinghua.edu.cn>
2016-02-27 21:02:03 +0800

    Add more keys.

--- GoogleBaseLoc/Program.cs
+++ GoogleBaseLoc/Program.cs
@@ -90,13 +90,25 @@ namespace GoogleBaseLoc
 
     class Program
     {
+        //private static string key = "AIzaSyCTPP_vSZaC-HDygwlij12UbVLVwVqYzsI";
+        //private static string key = "AIzaSyB32yWrWa5C11kt0XpHHS3V13NekRENzS4";
+        //private static string key = "AIzaSyC8vvERhzfR-HwI6hR_wqatVy5NSfwAxAk";
+        //private static string key = "AIzaSyCgWuezqquuwt1TzEO2IjCZOL9TGtEOOp8";
+        //private static string key = "AIzaSyDVPpVM4nNYWhGfYQtc574Qfrk6ixGYD0k";
+        //private static string key = "AIzaSyCGfsA1u-9GiKfYWZCjTAkp1XZzFrtTp0Y";
+        //private static string key = "AIzaSyDe705w27eoNvJp6gOVNx6MRav24hb8hhc";
+        //private static string key = "AIzaSyD7qRLfpYPsY_cTGyuUmgeD8alT2M0457w";
+        private static string[] keys = { "AIzaSyCTPP_vSZaC-HDygwlij12UbVLVwVqYzsI", "AIzaSyB32yWrWa5C11kt0XpHHS3V13NekRENzS4", "AIzaSyC8vvERhzfR-HwI6hR_wqatVy5NSfwAxAk", "AIzaSyCgWuezqquuwt1TzEO2IjCZOL9TGtEOOp8" , "AIzaSyDVPpVM4nNYWhGfYQtc574Qfrk6ixGYD0k" , "AIzaSyCGfsA1u-9GiKfYWZCjTAkp1XZzFrtTp0Y" , "AIzaSyDe705w27eoNvJp6gOVNx6MRav24hb8hhc", "AIzaSyD7qRLfpYPsY_cTGyuUmgeD8alT2M0457w",
+        "AIzaSyDEyq1Kw77pNb5uvZIpoyLmxmcAROO1NW8", "AIzaSyD0o9VxqGInY1g8bhifouw5quFm9q3J4do", "AIzaSyB3qC6aF5Rl7H32GT_lCHH6vbYNaY6kjio", "AIzaSyAESwcwYREqeCLbTUju0gY1Y_WSQAQlTTg", "AIzaSyACawcyKsUOI_SktQ8rVdk4SWZTt-YCO5w", " AIzaSyB1Je0Zu_328gxrKrSjfSRKDO6t2ZfDzXM", "AIzaSyA6zGS5zPVOG2CfIcz4Bu_NPeKRrLnFtrQ", "AIzaSyBYcUNDut2f_b1oaXhj_0-0EstEFJbIM2o" };
+        private static int keypos = 0;
+
         static void Main(string[] args)
         {
-            var conn = new MySqlConnection("server=localhost;user id=root;password=;database=pbsmirror;pooling=false");
+            var conn = new MySqlConnection("server=127.0.0.1;user id=root;password=;database=pbsmirror;pooling=false");
             conn.Open();
             var ser = new DataContractJsonSerializer(typeof(WifiQuery));
             var ser2 = new DataContractJsonSerializer(typeof(GoogleLocResponse));
-            using (var fs = new StreamReader("D:\\wifi\\wifiQuery.dat"))
+            using (var fs = new StreamReader("wifiQuery.dat"))
             {
                 while (!fs.EndOfStream)
                 {
@@ -114,7 +126,7 @@ namespace GoogleBaseLoc
                         }
                     foreach (var bs in query.bs)
                     {
-                        if (bs.id.Contains("null")) continue;
+                        if (bs.id.Contains("null") || bs.id.Contains("-") || bs.id.Contains("/") || bs.id.Contains("@")) continue;
                         var cmd = new MySqlCommand("SELECT COUNT(*) FROM googlebs WHERE id='" + bs.id + "'", conn);
                         var reader = cmd.ExecuteReader();
                         reader.Read();
@@ -137,7 +149,7 @@ namespace GoogleBaseLoc
                             HttpWebResponse res = null;
                             try
                             {
-                                req = (HttpWebRequest)WebRequest.Create(new Uri("https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyB32yWrWa5C11kt0XpHHS3V13NekRENzS4"));
+                                req = (HttpWebRequest)WebRequest.Create(new Uri("https://www.googleapis.com/geolocation/v1/geolocate?key=" + keys[keypos]));
                                 req.Method = "POST";
                                 req.ContentType = "application/json";
                                 var post = "{\"considerIp\": \"false\",\"cellTowers\":[{\"cellId\":" + cid + ",\"locationAreaCode\":" + lac + ",\"mobileCountryCode\":" + mcc + ",\"mobileNetworkCode\":" + mnc + "}]}";
@@ -165,12 +177,17 @@ namespace GoogleBaseLoc
                                 Console.Write(e.Message);
                                 if (res.StatusCode == HttpStatusCode.Forbidden)
                                 {
-                                    Console.WriteLine("No quota. Program terminates.");
-                                    return;
+                                    Console.WriteLine("No quota. Move to next key.");
+                                    keypos++;
+                                    if (keypos == keys.Length)
+                                    {
+                                        Console.WriteLine("All keys invalid.");
+                                        return;
+                                    }
                                 }
                                 else if (res.StatusCode == HttpStatusCode.NotFound)
                                 {
-                                    Console.Write("Not found.");
+                                    Console.Write(" ");
                                     tag = false;
                                 }
                                 else Console.WriteLine();
